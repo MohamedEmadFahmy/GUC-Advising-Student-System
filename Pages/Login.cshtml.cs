db1d33cd0220b52f2590a1a583372b951c4661a7
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-12-09 21:36:57 +0200

    transfer files from vs to vscode

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace Milestone_3.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public List<Instructor> Instructors = new List<Instructor>();
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;" +
+                    "Initial Catalog=Advising_System;" +
+                    "Integrated Security=True;" +
+                    "Encrypt=False";
+
+
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+
+                String sql = "SELECT * FROM Instructor";
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    Instructor instructor = new Instructor();
+                    instructor.id = "" + reader.GetInt32(0);
+                    instructor.name = reader.GetString(1);
+                    instructor.email = reader.GetString(2);
+                    instructor.faculty = reader.GetString(3);
+                    instructor.office = reader.GetString(4);
+
+
+                    Instructors.Add(instructor);
+                }
+
+                connection.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception:  " + e.ToString());
+            }
+
+        }
+    }
+
+
+
+
+    public class Instructor {
+        public string id;
+        public string name;
+        public string email;
+        public string faculty;
+        public string office;
+    }
+}
